cda0c3f5e1b4e3b5638e4423d9e07594ef4f1bab
Lucas <lucasmeinen@yahoo.com.br>
2022-10-13 10:20:52 -0300

    Laço for

--- Dia2/For/index.js
+++ Dia2/For/index.js
@@ -6,3 +6,12 @@ for (let index = 0; index < groceryList.length; index++) {
   const element = groceryList[index];
   console.log(element);
 }
+
+//Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let index of names) {
+  console.log(index);
+}
+
